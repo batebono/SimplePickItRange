03cc4fee27af6c48f7f2a211bbf8689914fcab38
Queuete <62324681+Queuete@users.noreply.github.com>
2021-05-25 19:05:12 +0200

    Remove unused setting option

--- SimplePickItSettings.cs
+++ SimplePickItSettings.cs
@@ -18,9 +18,6 @@ namespace SimplePickIt
         [Menu("Time between clicks in milliseconds")]
         public RangeNode<int> DelayClicksInMs { get; set; } = new RangeNode<int>(40, 0, 100);
 
-        [Menu("Delay between search items to pick")]
-        public RangeNode<int> DelayGetItemsToPick { get; set; } = new RangeNode<int>(200, 50, 500);
-
         [Menu("Activate extensive debug logging")]
         public ToggleNode DebugLogging { get; set; } = new ToggleNode(false);
 
